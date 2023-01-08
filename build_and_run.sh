9caf455c830c7433281886eefadca4d0669d5f1e
Thomas Lahmer <791550+Tomok@users.noreply.github.com>
2023-01-08 13:43:17 +0100

    added lldb debugging

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,4 +2,4 @@
 # stop any previously running instance
 docker stop rust-dev-container
 docker build -t rust-dev docker || exit -1
-docker run --name rust-dev-container -v $(pwd)/vm_workspace:/home/dev/workspace -it rust-dev
+docker run --name rust-dev-container -v $(pwd)/vm_workspace:/home/dev/workspace --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it rust-dev
